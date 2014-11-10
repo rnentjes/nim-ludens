21c1c4e612d3e336df291165ee2896663744d13c
rnentjes <rien@nentjes.com>
2014-11-10 20:15:05 +0100

    Add simple example

---
+++ examples/openwindow/readme.md
@@ -0,0 +1,6 @@
+Single file example showing:
+
+- Texture loading and displaying
+- Music loading and playing
+- Font rendering
+- Keyboard input
--- examples/shooter/src/shooter.nim
+++ examples/shooter/src/shooter.nim
@@ -1,20 +1,15 @@
 import csfml
-import opengl as gl
-import math
-
-import screen
 import game
-import texture
 
 import screen/splash as splash
 
 #
 
-var shooter = game.create(startScreen = splash.createSplash(), 
-                          title = "Nimvaders!", 
-                          vsync = true, 
-                          fullscreen = false, 
-                          width = 600, 
+var shooter = game.create(startScreen = splash.createSplash(),
+                          title = "Nimvaders!",
+                          vsync = true,
+                          fullscreen = false,
+                          width = 600,
                           height = 900)
 
 shooter.SetClearColor(color(40, 40, 40))
