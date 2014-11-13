ebca8dfee24d4318ecdfa0043377761a6569ee49
rnentjes <rien@nentjes.com>
2014-11-13 21:43:53 +0100

    Credits

--- examples/shooter/src/screen/splash.nim
+++ examples/shooter/src/screen/splash.nim
@@ -18,6 +18,13 @@ type
     time: float32
     text1Alpha: int
 
+var
+  texts1 = ["Music", "Graphics", "Game"]
+
+  texts2 = ["DeceasedSuperiorTechnician", "Kenney.nl", "Rien Nentjes"]
+  currentText = 0
+  textTime = 0'f32
+
 ##
 
 proc createSplash*(): SplashScreen =
@@ -41,6 +48,12 @@ method Dispose*(screen: SplashScreen) =
 
 method Update*(screen: SplashScreen, delta: float32) =
   screen.time += delta
+  textTime += delta
+
+  if textTime > 8:
+    textTime = 0
+    currentText += 1
+    currentText = currentText mod len(texts1)
 
   screen.text1Alpha = max(int(sin(screen.time * 3) * 255), 0)
 
@@ -48,6 +61,19 @@ method Update*(screen: SplashScreen, delta: float32) =
 method Render*(screen: SplashScreen) =
   screen.font.SetColor(color(255, 100, 0, screen.text1Alpha))
   screen.font.DrawCentered("Press Space to Start!", 32, 0'f32, 0'f32)
+
+  var textAlpha = 255;
+  if textTime < 3:
+    textAlpha = int((textTime - 2) * 255)
+    textAlpha = max(textAlpha, 0)
+  elif textTime > 7:
+    textAlpha = int((8-textTime) * 255)
+
+  screen.font.SetColor(color(0, 150, 255, textAlpha))
+  screen.font.DrawCentered(texts1[currentText], 24, 0'f32, -200'f32)
+  screen.font.DrawCentered(texts2[currentText], 24, 0'f32, -240'f32)
+
+
 
 
 
